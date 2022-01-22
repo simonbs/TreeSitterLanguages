101150adf50efa49a2023aeeefc0374f18ab6747
Simon Støvring <mail@simonbs.dk>
2022-01-22 15:58:21 +0100

    Fixes build issue

--- Sources/TreeSitterElixirRunestone/TreeSitterLanguage+Helpers.swift
+++ Sources/TreeSitterElixirRunestone/TreeSitterLanguage+Helpers.swift
@@ -5,6 +5,6 @@ import TreeSitterElixirQueries
 public extension TreeSitterLanguage {
     static var elixir: TreeSitterLanguage {
         let highlightsQuery = TreeSitterLanguage.Query(contentsOf: TreeSitterElixirQueries.Query.highlightsFileURL)
-        return TreeSitterLanguage(tree_sitter_elixir(), highlightsQuery: highlightsQuery, injectionsQuery: nil, indentationScopes: .elixir)
+        return TreeSitterLanguage(tree_sitter_elixir(), highlightsQuery: highlightsQuery, injectionsQuery: nil, indentationScopes: nil)
     }
 }
