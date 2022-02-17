980009a71e4b56178e2f2c914327d0c8375b0573
Simon Støvring <mail@simonbs.dk>
2022-02-17 19:30:04 +0100

    Improves highlighting of YAML

--- Sources/TreeSitterYAMLQueries/queries/highlights.scm
+++ Sources/TreeSitterYAMLQueries/queries/highlights.scm
@@ -1,9 +1,5 @@
 (boolean_scalar) @boolean
 (null_scalar) @constant.builtin
-(double_quote_scalar) @string
-(single_quote_scalar) @string
-(block_scalar) @string
-(string_scalar) @string
 (escape_sequence) @string.escape
 (integer_scalar) @number
 (float_scalar) @number
@@ -18,6 +14,13 @@
   key: (flow_node [(double_quote_scalar) (single_quote_scalar)] @field))
 (block_mapping_pair
   key: (flow_node (plain_scalar (string_scalar) @field)))
+(block_mapping_pair
+  value: (flow_node [(double_quote_scalar) (single_quote_scalar)] @string))
+(block_mapping_pair
+  value: (flow_node (plain_scalar (string_scalar) @string)))
+
+(block_sequence_item (flow_node [(double_quote_scalar) (single_quote_scalar)] @string))
+(block_sequence_item (flow_node (plain_scalar (string_scalar) @string)))
 
 (flow_mapping
   (_ key: (flow_node [(double_quote_scalar) (single_quote_scalar)] @field)))
