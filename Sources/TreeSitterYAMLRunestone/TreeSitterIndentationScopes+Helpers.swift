04b1e22f6cf4dd964a57756ea1b53d1966c369b1
Simon Støvring <mail@simonbs.dk>
2022-02-17 21:03:19 +0100

    Removes indentation for YAML

--- Sources/TreeSitterYAMLRunestone/TreeSitterIndentationScopes+Helpers.swift
+++
@@ -1,7 +0,0 @@
-import Runestone
-
-public extension TreeSitterIndentationScopes {
-    static var yaml: TreeSitterIndentationScopes {
-        return TreeSitterIndentationScopes(indent: ["block_mapping_pair"], whitespaceDenotesBlocks: true)
-    }
-}
