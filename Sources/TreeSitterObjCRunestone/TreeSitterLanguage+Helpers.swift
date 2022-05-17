d6cafd6ac4b83d1e4e7aae5aeec16206c7ba51f8
Rafael Francisco <rafaelfrancisco6@gmail.com>
2022-05-17 17:02:44 +0100

    Add ObjC support

---
+++ Sources/TreeSitterObjCRunestone/TreeSitterLanguage+Helpers.swift
@@ -0,0 +1,10 @@
+import Runestone
+import TreeSitterObjC
+import TreeSitterObjC
+
+public extension TreeSitterLanguage {
+    static var objc: TreeSitterLanguage {
+        let highlightsQuery = TreeSitterLanguage.Query(contentsOf: TreeSitterObjCQueries.Query.highlightsFileURL)
+        return TreeSitterLanguage(tree_sitter_objc(), highlightsQuery: highlightsQuery)
+    }
+}
