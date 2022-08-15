cbf2483f283f9596eb20c9df7887b543380ac18d
Nikita Shalin <dratemp16n@gmail.com>
2022-08-15 18:14:40 +0300

    lower deployment target

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TreeSitterLanguages",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(name: "TreeSitterLanguagesCommon", targets: ["TreeSitterLanguagesCommon"]),
         .library(name: "TreeSitterAstro", targets: ["TreeSitterAstro"]),
