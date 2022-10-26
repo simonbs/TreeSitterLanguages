d5282e0dc4bb040f09dcf907e876079268cc361d
Haldun Bayhantopcu <haldun@github.com>
2022-10-26 16:08:51 +0200

    Update Runestone to 0.2.7

--- Package.swift
+++ Package.swift
@@ -115,7 +115,7 @@ let package = Package(
         .library(name: "TreeSitterYAMLRunestone", targets: ["TreeSitterYAMLRunestone"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/simonbs/Runestone", from: "0.2.6")
+        .package(url: "https://github.com/simonbs/Runestone", from: "0.2.7")
     ],
     targets: [
         .target(name: "TreeSitterLanguagesCommon"),
